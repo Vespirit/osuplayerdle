3bb7e6259101ea32699d6e46138a86b02413bd33
Carmelo Restivo-Caponcello <carmrest00@gmail.com>
2023-11-08 11:06:18 -0500

    Solution choice now random and seeded by the current day.

--- src/components/playerdatagame/PlayerDataGame.tsx
+++ src/components/playerdatagame/PlayerDataGame.tsx
@@ -6,8 +6,16 @@ import { startOfToday } from 'date-fns'
 
 function PlayerDataGame() {
 
+  const getRandomInt = (min: number, max: number, seed: number) => {
+    min = Math.ceil(min);
+    max = Math.ceil(max);
+    let rfl = Math.sin(seed) * 10000;
+    rfl = rfl - Math.floor(rfl);
+    return Math.floor(rfl * (max - min) + min);
+  }
+
   const getSolution = () => {
-    return SOLUTIONS[startOfToday().getTime() % SOLUTIONS.length];
+    return SOLUTIONS[getRandomInt(0, SOLUTIONS.length, startOfToday().getTime()) % SOLUTIONS.length];
   }
 
   const SOLUTION: string = getSolution().toString();
@@ -46,7 +54,7 @@ function PlayerDataGame() {
 
   return (
     <>
-      <input value={inputText} onChange={handleInputChange}/>
+      <input type="text" placeholder="Guess a player..." value={inputText} onChange={handleInputChange} />
       <input type="submit" value="Guess!" onClick={handleInputSubmit} disabled={isGameOver} />
       <p>{msg}</p>
       <Table guesses={guessList} />
