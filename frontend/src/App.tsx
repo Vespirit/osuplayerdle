dac7bce9739f11f3882090a4e6d5572d6bfacee1
Carmelo Restivo-Caponcello <carmrest00@gmail.com>
2023-12-09 19:00:28 -0500

    Add initial browser router functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// components
 import Navbar from "./components/navbar/navbar";
 import PlayerDataGame from "./components/playerdatagame/PlayerDataGame";
 
 function App() {
 
   return (
-    <center>
-      <Navbar />
-      <PlayerDataGame />
-    </center>
+    <div className = "App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="Pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<PlayerDataGame />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
