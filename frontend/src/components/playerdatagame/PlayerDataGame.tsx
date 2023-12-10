8846dee4b5402912ad0579ddc99acdf210efab3d
Carmelo Restivo-Caponcello <carmrest00@gmail.com>
2023-12-09 22:26:46 -0500

    Switched solution determining to use the api

--- frontend/src/components/playerdatagame/PlayerDataGame.tsx
+++ frontend/src/components/playerdatagame/PlayerDataGame.tsx
@@ -1,38 +1,36 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import Table from "./gameboard/table";
-import { SOLUTIONS } from "../solutions";
 import { startOfToday } from "date-fns";
 import { getRandomInt } from "../../tools/random";
 
 function PlayerDataGame() {
 
-  const [players, setPlayers] = useState<any[]>([]);
+  const [playerList, setPlayerList] = useState<any>([]);
+  const [solution, setSolution] = useState("");
   const [inputText, setInputText] = useState<string>("");
   const [guessList, setGuessList] = useState<string[]>([]);
   const [isGameOver, setIsGameOver] = useState<boolean>(false);
   const [attempts, setAttempts] = useState<number>(1);
   const [msg, setMsg] = useState<string>("");
 
-  useEffect(() => {
+  useEffect(() => { // fetch player list from api
     const fetchPlayers = async () => {
       const response = await fetch('/api/players');
       const json = await response.json();
 
       if (response.ok) {
-        setPlayers(json);
+        setPlayerList(json);
+        setSolution(json[
+          getRandomInt(0, json.length, startOfToday().getTime())
+        ].username);
       }
       else {
         setMsg("error response not ok")
       }
     }
-
     fetchPlayers()
-  }, [])
-
-  const SOLUTION: string = SOLUTIONS[
-    getRandomInt(0, SOLUTIONS.length, startOfToday().getTime())
-  ].toString();
+  }, []);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value);
@@ -46,12 +44,12 @@ function PlayerDataGame() {
     if (guessList.includes(curGuess)) return;
     setGuessList([...guessList, curGuess]);
 
-    if (curGuess === SOLUTION) {
+    if (curGuess === solution) {
       setIsGameOver(true);
       setMsg("Well done! Got it in " + attempts.toString() + " attempt(s).");
     } else if (attempts >= 6) {
       setIsGameOver(true);
-      setMsg("Game over. Correct answer: " + SOLUTION);
+      setMsg("Game over. Correct answer: " + solution);
     } else {
       setInputText("");
       setAttempts(attempts + 1);
@@ -76,7 +74,7 @@ function PlayerDataGame() {
       <Table guesses={guessList} />
       {/*<p>{inputText},{attempts},{isGameOver.toString()},{guessList}</p> debug output*/}
       <div className="Players">
-        {players && players.map((player) => (
+        {playerList && playerList.map((player: { _id: React.Key | null | undefined; username: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }) => (
           <p key={player._id}>{player.username}</p>
         ))}
       </div>
