b2881761602eb4f3b5431f1c3056397f238e5f60
Carmelo Restivo <carmrest00@gmail.com>
2024-01-07 13:00:02 -0500

    Add spaces between values and hints

--- frontend/src/components/games/playerdatagame/gameboard/table.tsx
+++ frontend/src/components/games/playerdatagame/gameboard/table.tsx
@@ -26,11 +26,15 @@ function Table({ guesses, guessProps, guessHints }: Props) {
                     return (
                         <Row
                             guessNum={(i + 1).toString()}
-                            id={guessProps.id + guessHints[i][3]}
+                            id={guessProps.id + " " + guessHints[i][3]}
                             username={guessProps.username}
-                            country={guessProps.country + guessHints[i][1]}
-                            rank={guessProps.rank + guessHints[i][0]}
-                            playcount={guessProps.playcount + guessHints[i][2]}
+                            country={
+                                guessProps.country + " " + guessHints[i][1]
+                            }
+                            rank={guessProps.rank + " " + guessHints[i][0]}
+                            playcount={
+                                guessProps.playcount + " " + guessHints[i][2]
+                            }
                         />
                     );
                 })}
